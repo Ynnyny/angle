36ed23b7306a2fd4607e96554c02ef6e93338d0b
Ynnyny <ynnyny@users.noreply.github.com>
2026-08-14 05:03:46 -0700

    fix: link CoreFoundation.framework for is_ios in bundled vulkan-loader (CFRelease/CFBundle* undefined)

--- build-angle-ios-vulkan.sh
+++ build-angle-ios-vulkan.sh
@@ -57,7 +57,9 @@ which ninja >/dev/null 2>&1 || brew install ninja
 # Patch vulkan-loader GN: it only defines SYSCONFDIR/FALLBACK_* for
 # linux/chromeos/mac; iOS needs them too (ANGLE iOS upstream is Metal-only,
 # so this never ran there).
-sed -i '' 's/if (is_linux || is_chromeos || is_mac) {/if (is_linux || is_chromeos || is_mac || is_ios) {/' \
+sed -i '' \
+    -e 's/if (is_linux || is_chromeos || is_mac) {/if (is_linux || is_chromeos || is_mac || is_ios) {/' \
+    -e 's/if (is_mac) {/if (is_mac || is_ios) {/' \
     third_party/vulkan-loader/src/BUILD.gn
 
 CURRENT_ANGLE_COMMIT=$(git rev-parse HEAD)
